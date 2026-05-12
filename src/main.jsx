8c1cea9a7a134155ebaa456436511d0eecaf6ef7
susmithdion7-oss <susmithdion7@gmail.com>
2026-05-13 06:07:11 +0800

    Import Squad Hub styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './tacticalBoard.css'
 import './teamWizard.css'
 import './crest.css'
 import './dashboard.css'
+import './playersHub.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
